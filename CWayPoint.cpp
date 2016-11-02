c96d11dd513868217d45658cdd2c3baafbe27a75
Andi <a.vetterlein@strictmobile.de>
2016-11-02 23:36:39 +0100

    init

---
+++ CWayPoint.cpp
@@ -0,0 +1,18 @@
+/*
+ * CWayPoint.cpp
+ *
+ *  Created on: 02.11.2016
+ *      Author: Andi
+ */
+
+#include "CWayPoint.h"
+
+CWayPoint::CWayPoint() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CWayPoint::~CWayPoint() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CWayPoint.h
@@ -0,0 +1,17 @@
+/*
+ * CWayPoint.h
+ *
+ *  Created on: 02.11.2016
+ *      Author: Andi
+ */
+
+#ifndef CWAYPOINT_H_
+#define CWAYPOINT_H_
+
+class CWayPoint {
+public:
+	CWayPoint();
+	virtual ~CWayPoint();
+};
+
+#endif /* CWAYPOINT_H_ */
